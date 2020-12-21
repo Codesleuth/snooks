3bec66bf22f493be6743b6bd71dbadfa39f5a6ca
David Wood <david.p.wood@gmail.com>
2020-12-21 13:37:32 +0000

    remove hooks folder

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as useNowFactory } from './hooks/useNowFactory'
-export { default as usePredictableTrigger } from './hooks/usePredictableTrigger'
-export { default as useTrigger } from './hooks/useTrigger'
+export { default as useNowFactory } from './useNowFactory'
+export { default as usePredictableTrigger } from './usePredictableTrigger'
+export { default as useTrigger } from './useTrigger'
